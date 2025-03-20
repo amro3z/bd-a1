4f5cb54f6db10c2b9f4912c6ad74c4234e2e0891
Amr Hamouda <A.hamouda2276@nu.edu.eg>
2025-03-20 19:41:54 +0200

    Updated Docker image and code

--- bd-a1/service-result/dpre.py
+++ bd-a1/service-result/dpre.py
@@ -6,20 +6,12 @@ file_path = sys.argv[1]
 
 df = pd.read_csv(file_path, encoding='utf-8')
 df.drop(columns=['Ticket', 'Cabin', 'Name' , 'PassengerId'], errors='ignore', inplace=True)
-df.columns = df.columns.str.strip().str.lower()
-df.fillna(df.median(numeric_only=True), inplace=True)
-df.drop_duplicates(inplace=True)
+duplicates = df[df.duplicated()]
 duplicates_count = df.duplicated().sum()
-print(f"Num of Duplicates Columns {duplicates_count}")
-
-for col in df.select_dtypes(include=['object']).columns:
-    df[col] = df[col].str.lower()
-
-print(f" Num of Columns after lowercase: {df.shape[0]}")
-
-df.to_csv("res_dpre.csv", index=False)
-
+print(f"Num of Columns after Drop {df.shape[0] - duplicates_count}")
+df.drop_duplicates(inplace=True)
 print("Preprocess Done")
 
+df.to_csv("res_dpre.csv", index=False)
 subprocess.run(["python3", "/home/doc-bd-a1/eda.py", "/home/doc-bd-a1/res_dpre.csv"])
 
--- bd-a1/service-result/vis.py
+++ bd-a1/service-result/vis.py
@@ -8,13 +8,11 @@ file_path = sys.argv[1]
 df = pd.read_csv(file_path)
 
 plt.figure(figsize=(8, 6))
-
-sns.boxplot(data=df, x="survived", y="fare", palette="Set2")
-
-plt.xlabel("Survived (0 = No, 1 = Yes)")
-plt.ylabel("Ticket Fare")
-plt.title("Ticket Fare Distribution by Survival")
-
+sns.histplot(df, x="age", hue="survived", kde=True, bins=30)
+plt.title("Age Distribution by Survival")
+plt.xlabel("Age")
+plt.ylabel("Count")
+plt.show()
 plt.tight_layout()
 plt.savefig("vis.png")
 
